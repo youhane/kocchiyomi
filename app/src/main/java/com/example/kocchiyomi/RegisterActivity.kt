69e4f9770779cbb8ac164e41ecee18e85b203988
mgradyn <mgradyn@gmail.com>
2022-11-13 22:57:26 +0700

    [Add] Firebase Registration Auth

--- app/src/main/java/com/example/kocchiyomi/RegisterActivity.kt
+++ app/src/main/java/com/example/kocchiyomi/RegisterActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kocchiyomi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -17,46 +19,73 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         btn_signup.setOnClickListener {
+            val email: String = et_signup_email.text.toString().trim { it <= ' '}
+//            val username: String = et_signup_email.text.toString().trim { it <= ' '}
+            val password: String = et_signup_password.text.toString().trim { it <= ' '}
+            val confirmPassword: String = et_signup_password.text.toString().trim { it <= ' '}
+
             when {
-                TextUtils.isEmpty(et_signup_username.text.toString().trim() {it <= ' '}) -> {
-                    Toast.makeText(
-                        this@RegisterActivity,
-                        "Please enter username",
-                        Toast.LENGTH_SHORT
-                    ).show()
+//                TextUtils.isEmpty(username) -> {
+//                    makeToast("Please enter username")
+//                }
+
+                TextUtils.isEmpty(email) -> {
+                    makeToast("Please enter email")
                 }
 
-                TextUtils.isEmpty(et_signup_email.text.toString().trim {it <= ' '}) -> {
-                    Toast.makeText(
-                        this@RegisterActivity,
-                        "Please enter email",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                isValidEmail(email) -> {
+                    makeToast("Please enter valid email")
                 }
 
-                TextUtils.isEmpty(et_signup_email.text.toString().trim {it <= ' '}) -> {
-                    Toast.makeText(
-                        this@RegisterActivity,
-                        "Please enter email",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                TextUtils.isEmpty(password) -> {
+                    makeToast("Please enter password")
                 }
 
-                TextUtils.isEmpty(et_signup_email.text.toString().trim {it <= ' '}) -> {
-                    Toast.makeText(
-                        this@RegisterActivity,
-                        "Please enter email",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                TextUtils.isEmpty(confirmPassword) -> {
+                    makeToast("Please confirm password")
+                }
+
+                TextUtils.equals(password, confirmPassword) -> {
+                    makeToast("The password does not match")
                 }
 
                 else -> {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> {
+                                task ->
+                                    if(task.isSuccessful){
+                                        val firebaseUser: FirebaseUser = task.result!!.user!!
 
+                                        makeToast("You are registered successfully")
+
+                                        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        intent.putExtra("user_id", firebaseUser.uid)
+                                        intent.putExtra("email_id", email)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        makeToast(task.exception!!.message.toString())
+                                    }
+                            }
+                        )
                 }
 
             }
         }
 
 
+    }
+    private fun isValidEmail(str: CharSequence):Boolean{
+        return Patterns.EMAIL_ADDRESS.matcher(str).matches()
+    }
+
+    private fun makeToast(msg: String) {
+        Toast.makeText(
+            this@RegisterActivity,
+            msg,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
